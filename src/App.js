b5e800707571f82e6eb3fb2e19e43f622b770133
dmitrenkoyi <artemovayi@gmail.com>
2021-07-27 20:14:30 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { Component } from 'react';
+
+import Container from './components/Container';
+import Section from './components/Section';
+import FeedbackOptions from './components/FeedbackOptions';
+import Statistics from './components/Statistics';
+
+class App extends Component {
+  // static defaultProps = {
+  //   total: 0,
+  //   positivePercentage: 0,
+  // };
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, value) => (acc += value), 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const positivePercent = Math.round(
+      (this.state.good * 100) / this.countTotalFeedback(),
+    );
+    return positivePercent || 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          ></Statistics>
+        </Section>
+      </Container>
+    );
+  }
+}
+
+export default App;
